a124ed22bb09ad40b9ea41a68dcf87d69ca5afb9
Víctor Ferrer García <victorfergar@gmail.com>
2019-10-16 22:57:35 +0200

    Update facts

--- languageStrings.js
+++ languageStrings.js
@@ -2,17 +2,22 @@ var languageStrings = {
     "en": {
         "translation": {
             "FACTS": [
-                "Llamas are large animals. They can be 6 feet tall and reach between 280 and 450 pounds.",
-                "A Llama's body is covered with wool which can be black, gray, white or brown, with variety of patterns.",
-                "Llamas have excellent sight, smell and hearing, which are used for detection of potential danger.",
-                "Llamas are very social animals. They live in groups composed of up to 20 animals.",
-                "Llamas are known as animals that spit when provoked or threatened. They can spit at humans also, but they usually spit at each other to solve misunderstandings in the group. ",
-                "Llamas are very intelligent, but stubborn animals. When the load on its back is too heavy, a llama will refuse to carry it until some of the load is removed.",
-                "The lifespan of a llama is between 20 and 30 years."
+                "Seville is home to some well-known scenes in big films. The classic 1962 Lawrence of Arabia was filmed in locations across the city, you will recognise ‘The officers club in Cairo’, ‘Jerusalem’ and the ‘Council chamber’ between many others",
+                "Many Spanish regions individually claim to be the birthplace of Tapas, arguably Seville holds the strongest claim.",
+                "Home to some of Europe’s most colourful festivals and Feria de Abril fall within two weeks of each other. Festival Semana Santa is a hugely holy festival in Seville, the streets come alive with processions to commemorate the passion of Christ. Usually taking place a week before Easter it is a major celebration in Spain. Two weeks later the city hosts Feria de Abril a week long spring fair with dancing, drinking, eating and socialising.",
+                "The impressive Seville Cathedral is one of the largest in the world. After his death in 1506 Columbus was buried first in Valladolid Spain, then taken to Monasterio La Cartuja in Sevilla. The remains were again moved to what is now the Dominican Republic before Spain lost control and was moved to Havana Cuba before returning to rest in Seville after Cuban independence in 1898.",
+                "Some Sevilla streets are full of orage trees, but those orange are bitter high in vitamin C and perfect for making marmalade.",
+                "According to legend the secret code NO8DO was given to the city by King Alfonso X. You will catch glimpses of it all around the city and is said to translate to ‘it has not abandoned me’ (meaning the city of Seville).",
+                "A very popular momument called metropol parasolis has been designed by Jurgen Mayer the Metropol Parasolis claims to be the largest wooden structure in the world.",
+                "Seville is blessed with year round sunny weather. In fact, there is an average of only 80 days of rain every year. It is also the sunniest city in Europe with very hot summers, reaching highs of 35°C. In fact, the highest temperature ever recorded in Europe was in Seville in 1881 at 50°C!",
+                "Seville is a city of clean energy with a solar power tower providing clean and renewable energy to the city. In fact the tower is the world’s first commercial solar power tower with more towers planned.",
+                "Sevilla is the Home to 2 football clubs, famous across the world ‘Sevilla Futbol Club’ or simply ‘Sevilla FC’, is Spain’s oldest sporting club devoted to football. However they are no Seville’s only team. Some locals support Real Betis Balompie. Both football teams have a famous rivalry and often play each other in Seville’s stadium ‘Ramon Sanchez Pizjuan’",
+                "Sevillanos are very well-known, throughout Spain and the wider world, for their fierce pride in their city.",
+                "Seville is a Spanish city, the capital of the autonomous community of Andalusia and the province of Seville"
             ],
-            "SKILL_NAME" : "LLama Facts",
+            "SKILL_NAME" : "sevilla expert",
             "GET_FACT_MESSAGE" : "Here's your fact: ",
-            "HELP_MESSAGE" : "You can say tell me a llama fact, or, you can say exit... What can I help you with?",
+            "HELP_MESSAGE" : "You can say tell me a sevilla fact, or, you can say exit... What can I help you with?",
             "HELP_REPROMPT" : "What can I help you with?",
             "STOP_MESSAGE" : "Goodbye!"
         }
